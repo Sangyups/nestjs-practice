543fb6fe32079f7bfd96f1b1d1b0d2408659a980
Sangyups <feels4637@gmail.com>
2022-08-17 00:39:01 +0900

    feat: inject services to ComputerController

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { CpuService } from '@src/cpu/cpu.service';
+import { DiskService } from '@src/disk/disk.service';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController {
+  constructor(
+    private cpuService: CpuService,
+    private diskService: DiskService,
+  ) {}
+}
