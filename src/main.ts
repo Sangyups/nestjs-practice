0d67c891c5889bd6a2decf95066bae19d22288e0
Sangyups <feels4637@gmail.com>
2022-08-21 17:35:01 +0900

    feat: add ValidationPipes

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import appConfig from 'src/config';
 import { AppModule } from './app.module';
@@ -5,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(appConfig.APP_PORT);
 }
 bootstrap();
