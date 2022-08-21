8e282b44a7205d25b47e4b83fd74ae22829b5af0
Sangyups <feels4637@gmail.com>
2022-08-21 16:21:59 +0900

    feat: add database settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import appConfig from 'src/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: appConfig.DB_NAME,
+      host: appConfig.DB_HOST,
+      port: appConfig.DB_PORT,
+      username: appConfig.DB_USERNAME,
+      password: appConfig.DB_PASSWORD,
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
