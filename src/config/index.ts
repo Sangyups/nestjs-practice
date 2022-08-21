012b74fce8d68c5b7cfb2385cd24b2c020bbd897
Sangyups <feels4637@gmail.com>
2022-08-21 17:22:54 +0900

    chore: add port settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 
 const appConfig = {
+  // Application config
+  APP_PORT: +(process.env.APP_PORT || '4000'),
+
+  // DATABASE config
   DB_HOST: process.env.DB_HOST || '',
   DB_NAME: process.env.DB_NAME || '',
   DB_PORT: +(process.env.DB_PORT || '3306'),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import appConfig from 'src/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  await app.listen(appConfig.APP_PORT);
 }
 bootstrap();
